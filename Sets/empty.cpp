0d923bef1b6fb26178e39d1292b19f6a2315a8d8
Ananth <mvananthkumar@gmail.com>
2021-11-13 12:10:33 +0530

    Create empty.cpp

---
+++ Sets/empty.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+#include<set>
+
+using namespace std;
+
+int main(){
+	set<char> myset{'A', 'b'};
+	
+	if(myset.empty()){
+		cout<<"True";
+	}
+	else{
+		cout<<"False";
+	}
+
+
+    return 0;
+}
+
+
