6cd0955ad40f433e9abc5e0710c022f28cd0aef0
Ananth <mvananthkumar@gmail.com>
2022-03-14 15:09:48 +0530

    Create fill-2.cpp

---
+++ Lists/fill-2.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+  
+int main()
+{
+    int arr[10];
+  
+    fill(arr, arr + 10, 4);
+  
+    for (int i = 0; i < 10; i++)
+        cout << arr[i] << " ";
+  
+    return 0;
+}
