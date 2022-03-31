977eee23b048c53bef86b02b39c052d0a8ad2d6b
Ananth <mvananthkumar@gmail.com>
2022-03-31 20:09:14 +0530

    Create max_size.cpp

---
+++ Lists/max_size.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+#include<list>
+using namespace std;
+
+int main(){
+	
+	list<int> list1;
+  
+    // checking the max size of the list
+    cout <<list1.max_size();
+
+
+    return 0;
+}
+
+
