869cf39c0a9f47cc9474561737b9a37fe3be5d8c
Ananth <mvananthkumar@gmail.com>
2022-03-31 20:06:47 +0530

    Create reverse.cpp

---
+++ Lists/reverse.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <list>
+using namespace std;
+  
+int main()
+{
+    list<int> mylist{ 1, 2, 3, 4, 5 };
+    mylist.reverse();
+  
+    for (auto it = mylist.begin(); it != mylist.end(); ++it)
+        cout << ' ' << *it;
+}
+
