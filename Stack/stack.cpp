b15f71446cd233d578536f9177c0f7cf3bb330c6
Ananth <mvananthkumar@gmail.com>
2022-04-11 20:46:54 +0530

    Create stack.cpp

---
+++ Stack/stack.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+#include<stack>
+
+using namespace std;
+
+int main(){
+	
+	stack<int> s;
+	
+	s.push(1);
+	s.push(2);
+	s.push(3);
+	s.push(4);
+	
+	s.pop();
+	s.pop();
+	
+	while(!s.empty()){
+		cout<<s.top()<<" ";
+		s.pop();
+	}
+
+    return 0;
+}
+
+
