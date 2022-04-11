97b532eb24c49294efbba758e43dd93e9fc6cf8a
Ananth <mvananthkumar@gmail.com>
2022-04-11 20:46:50 +0530

    Create size.cpp

---
+++ Stack/size.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+#include<stack>
+
+using namespace std;
+
+int main(){
+	
+	stack<int> s;
+	
+	s.push(1);
+	s.push(2);
+	s.push(3);
+	s.push(4);
+	
+	cout<<s.size();
+
+
+    return 0;
+}
+
+
